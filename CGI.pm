88b0723f6da5d285502170ee8fc95c2c43e05f9c
lstein <lstein>
2007-04-16 16:58:46 +0000

    this will be version 3.29

--- CGI.pm
+++ CGI.pm
@@ -18,8 +18,8 @@ use Carp 'croak';
 # The most recent version and complete docs are available at:
 #   http://stein.cshl.org/WWW/software/CGI/
 
-$CGI::revision = '$Id: CGI.pm,v 1.233 2007-04-16 16:54:42 lstein Exp $';
-$CGI::VERSION='3.28';
+$CGI::revision = '$Id: CGI.pm,v 1.234 2007-04-16 16:58:46 lstein Exp $';
+$CGI::VERSION='3.29';
 
 # HARD-CODED LOCATION FOR FILE UPLOAD TEMPORARY FILES.
 # UNCOMMENT THIS ONLY IF YOU KNOW WHAT YOU'RE DOING.
