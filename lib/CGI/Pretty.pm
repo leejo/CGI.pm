49c35d825ec9d86efcc7a89c5f66f89949f33ed2
Mark Stosberg <mark@stosberg.com>
2009-08-17 18:36:06 -0400

    New Changes entry:     In CGI::Pretty, performance concerns are now documented. Thanks to Jochen, Rhesa Rozendaal and Mark Stosberg (RT#13223)

--- lib/CGI/Pretty.pm
+++ lib/CGI/Pretty.pm
@@ -249,6 +249,15 @@ now produces the following output:
        </TR>
     </TABLE>
 
+=head2 Recommendation for when to use CGI::Pretty
+
+CGI::Pretty is far slower than using CGI.pm directly. A benchmark showed that
+it could be about 10 times slower. Adding newslines and spaces may alter the
+rendered appearance of HTML. Also, the extra newlines and spaces also make the
+file size larger, making the files take longer to download. 
+
+With all those considerations, it is recommended that CGI::Pretty be used
+primarily for debugging. 
 
 =head2 Tags that won't be formatted
 
