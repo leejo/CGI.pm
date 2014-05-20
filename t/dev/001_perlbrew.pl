01a1ac8b22d94e55d975b9d6fb7c6dbd0b952866
Lee Johnson <lee@givengain.ch>
2014-05-20 08:57:18 +0200

    simplify perlbrew testing script
    just call make clean, perl Makefile.PL and make test. Makefile.PL
    requires 5.8 so no point testing any perls before that

--- t/dev/001_perlbrew.pl
+++ t/dev/001_perlbrew.pl
@@ -16,21 +16,24 @@ $ENV{DEV_TESTS}
 my $perlbrew = which( 'perlbrew' )
 	or plan skip_all => "perlbrew not found";
 
-# check available perls and all installed (except < 5.6.2)
+# check available perls and all installed
 my @perls = `$perlbrew available`;
 
-foreach my $perl ( @perls ) {
+foreach my $perl ( reverse @perls ) {
 
 	chomp( $perl );
 
 	if ( $perl =~ s/^i perl-// ) {
+
+		# we skip perls before 5.8 as Makefile.PL requires minimum 5.8
+		next if $perl =~ /^5\.[0-7]\./;
 
 		# yes, backticks...
 		# should probably fix this to use App::perlbrew module
 		`make clean`;
 		`$perlbrew exec --with $perl perl Makefile.PL`;
 		$? ? fail( "$perl Makefile.PL" ) : pass( "$perl Makefile.PL" );
-		`$perlbrew exec --with $perl perl "-MExtUtils::Command::MM" "-e" "test_harness(0, 'blib/lib', 'blib/arch')", t/*.t t/headers/*.t`;
+		`$perlbrew exec --with $perl make test`;
 		
 		$? ? fail( "$perl make test" ) : pass( "$perl make test" );
 	}
