b965c2aca1dc8ccc69ed8008ef5869e17148d16f
Lee Johnson <lee@givengain.ch>
2015-04-20 09:13:49 +0200

    Changes and VERSION bump for v4.15 release

--- lib/CGI/Pretty.pm
+++ lib/CGI/Pretty.pm
@@ -4,7 +4,7 @@ use strict;
 use if $] >= 5.019, 'deprecate';
 use CGI ();
 
-$CGI::Pretty::VERSION = '4.14_01';
+$CGI::Pretty::VERSION = '4.15';
 $CGI::DefaultClass = __PACKAGE__;
 @CGI::Pretty::ISA = qw( CGI );
 
