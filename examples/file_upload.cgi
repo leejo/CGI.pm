leejo/CGI.pm

commit d0497dbccde992c5ab5365a39e4516058e0c940f

Taint mode blocks vulnerability
If filename is `ARG`, the while loop will call `open()` on every thing in `@ARG` creating a remote execution vulnerability. Enabling taint mode prevents this. See my [article](http://perltricks.com/article/netanel-rubins-perljam-circus/http://perltricks.com/article/netanel-rubins-perljam-circus/) for details.
Another way to fix would be to use the double diamond operator `<<$file>>` will not call `open()`. But it requires Perl 5.22.0 or higher, which most people won't have.

examples/file_upload.cgi
@@ -1,4 +1,4 @@
-#!/usr/bin/env perl
+#!/usr/bin/env perl -T
 
 use strict;
 use warnings;

commit 309e14f10c924a7e982d88390927f5caa8357dda

fix t/examples.t for file_upload.cgi
broken by d0497dbccde992c5ab5365a39e4516058e0c940f as -T was added
to the shebang of file_upload.cgi but not to the command line run
in the t/examples.t test

examples/file_upload.cgi
@@ -15,6 +15,8 @@ my $template_vars = {
 # Process the form if there is a file name entered
 if ( my $file = $cgi->param( 'filename' ) ) {
 
+	die "filename passed as ARG" if $file =~ /ARG/;
+
     my $tmpfile  = $cgi->tmpFileName( $file );
     my $mimetype = $cgi->uploadInfo( $file )->{'Content-Type'} || '';
 

t/examples.t
@@ -52,7 +52,7 @@ foreach my $example ( sort keys( %examples ) ) {
 	foreach my $args ( sort keys( %{ $examples{$example} } ) ) {
 		foreach my $regexp ( @{ $examples{$example}{$args} } ) {
 			stdout_like(
-				"$^X $Bin/../examples/$example $args",
+				"$^X -T $Bin/../examples/$example $args",
 				$regexp,
 				$example
 			);