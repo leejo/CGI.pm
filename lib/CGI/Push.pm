f718c0cddb4de4efb94d8e55f41cd573d3cccda6
Lee Johnson <lee@givengain.ch>
2020-02-03 15:48:32 +0100

    bump VERSION and Changes for CPAN release

--- lib/CGI/Push.pm
+++ lib/CGI/Push.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 #/;
 
-$CGI::Push::VERSION='4.45';
+$CGI::Push::VERSION='4.46';
 use CGI;
 use CGI::Util 'rearrange';
 @ISA = ('CGI');
--- lib/Fh.pm
+++ lib/Fh.pm
@@ -5,6 +5,6 @@ package Fh;
 use strict;
 use warnings;
 
-$Fh::VERSION = '4.45';
+$Fh::VERSION = '4.46';
 
 1;
